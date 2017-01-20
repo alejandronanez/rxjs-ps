b31e996fdbddb282add730df028e99c08d7c9afd
Alejandro Nanez <alejonanez@gmail.com>
2017-01-19 21:16:35 -0500

    Moving mouse

--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
-console.log('yo');
+import { Observable } from 'rxjs';
+
+let circle = document.getElementById('circle');
+
+let source = Observable
+	.fromEvent(document, 'mousemove')
+	.map((e : MouseEvent) => ({
+		x: e.clientX,
+		y: e.clientY
+	}))
+	.filter(value => value.x < 500)
+	.delay(300);
+
+function onNext({x , y}) {
+	circle.style.left = x;
+	circle.style.top = y;
+}
+
+source.subscribe(
+	onNext,
+	e => console.log(`Error: ${e}`),
+	() => console.log(`...Completed...`)
+);
