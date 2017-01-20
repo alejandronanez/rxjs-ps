f6e87883daed50c4da91f66ee4495167bb241a8a
Alejandro Nanez <alejonanez@gmail.com>
2017-01-19 21:50:01 -0500

    Retrying on error

--- main.ts
+++ main.ts
@@ -3,28 +3,54 @@ import { Observable } from 'rxjs';
 const output = document.getElementById('output');
 const button = document.getElementById('button');
 
-let click = Observable
+const click = Observable
 	.fromEvent(button, 'click');
 
 function load(url: string) {
-	const xhr = new XMLHttpRequest();
+	return Observable.create(observer => {
+		const xhr = new XMLHttpRequest();
 
-	xhr.addEventListener('load', () => {
-		let movies = JSON.parse(xhr.responseText);
-		movies.forEach(m => {
-			let div = document.createElement('div');
-			div.innerText = m.title;
-			output.appendChild(div);
+		xhr.addEventListener('load', () => {
+			if (xhr.status === 200) {
+				const data = JSON.parse(xhr.responseText);
+				observer.next(data);
+				observer.complete();
+			} else {
+				observer.error(xhr.statusText);
+			}
 		});
-	});
 
-	xhr.open('GET', url);
-	xhr.send();
+		xhr.open('GET', url);
+		xhr.send();
+	})
+	.retryWhen(retryStrategy({ attempts: 3, delay: 1500 }));
 }
 
+function retryStrategy({ attempts = 4, delay = 1000 }) {
+	return function(errors) {
+		return errors
+			.scan((acc, value) => {
+				console.log(acc, value);
 
-click.subscribe(
-	e => load('movies.json'),
-	e => console.log(`Error: ${e}`),
-	() => console.log(`...Completed...`)
-);
+				return acc + 1;
+			}, 0)
+			.takeWhile(acc => acc < attempts)
+			.delay(delay);
+	}
+}
+
+function renderMovies(movies) {
+	movies.forEach(m => {
+		let div = document.createElement('div');
+		div.innerText = m.title;
+		output.appendChild(div);
+	});
+}
+
+click
+	.flatMap(e => load('movies.json'))
+	.subscribe(
+		renderMovies,
+		e => console.log(`Error: ${e}`),
+		() => console.log(`...Completed...`)
+	);
